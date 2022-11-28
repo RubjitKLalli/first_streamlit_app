c64caed1e2ece6168126183d1f1b2614315c3166
Rubjit Kaur Lalli <88930991+RubjitKLalli@users.noreply.github.com>
2022-11-28 21:14:36 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal 🥣')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie 🥗')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg 🐔')
 streamlit.text('🥑🍞 Avocado Toast 🥑🍞')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
 
  
