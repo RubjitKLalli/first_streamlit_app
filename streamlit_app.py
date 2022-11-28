65ecc10c48446138234bb09630e6d608e5f11200
Rubjit Kaur Lalli <88930991+RubjitKLalli@users.noreply.github.com>
2022-11-28 21:37:30 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,6 @@ mfl = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_m
 
 mfl = mfl.set_index('Fruit')
 
-streamlit.multiselect("Pick some fruits:", list(mfl.index),['Strawberries','Avocado'])
-
-streamlit.dataframe(mfl) 
+fs = streamlit.multiselect("Pick some fruits:", list(mfl.index),['Strawberries','Avocado'])
+fts = mfl.loc[fruits_selected]
+streamlit.dataframe(fs) 
